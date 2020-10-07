f34fdff7aaa5767d42c59755f487afe19b4fb47b
3062prachigupta <72510673+3062prachigupta@users.noreply.github.com>
2020-10-07 20:14:49 +0530

    my assignment of swapping two numbers without using a temporary variable.

---
+++ swapping two numbers.cpp
@@ -0,0 +1,11 @@
+#include<Stdio.h>
+int main()
+{
+int x, y;
+printf("please enter a number: \n");
+scanf("%d", &x);
+printf("please enter another no. for swaping:\n");
+scanf("%d", &y);
+printf("your swaping numbers are: %d, %d", y, x);
+return 0;
+}
