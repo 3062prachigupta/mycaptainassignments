74dcd44bf184bb8ee031299bc6b709202031628e
3062prachigupta <72510673+3062prachigupta@users.noreply.github.com>
2020-11-22 16:51:19 +0530

    Add files via upload

---
+++ product of two float no. in cpp.cpp
@@ -0,0 +1,14 @@
+//to find the size of int float double char in my pc 
+#include<iostream>
+using namespace std;
+int main()
+{
+	float num1, num2, product;
+	cout<<"enter two number:\n";
+	cin>>num1;
+	cin>>num2;
+	product= num1*num2;
+	cout<<"\nproduct ="<<product;
+	return 0;
+	
+}
