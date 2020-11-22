a0eefb69dab97ba916ad9d2b3cea35617ca6078b
3062prachigupta <72510673+3062prachigupta@users.noreply.github.com>
2020-11-22 16:51:40 +0530

    Add files via upload

---
+++ size of data types in cpp.cpp
@@ -0,0 +1,11 @@
+// to identify the size of int float char double
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"size of char:    "<<sizeof(char)<<" bytes\n"
+		<<"size of int:     "<<sizeof(int)<<" bytes\n"
+		<<"size of float:   "<<sizeof(float)<<" bytes\n"
+		<<"size of double:  "<<sizeof(double)<<" bytes\n";
+		return 0;
+}
